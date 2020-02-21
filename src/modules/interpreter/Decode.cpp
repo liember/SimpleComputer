0eb279ea72bb5eb18b0e25407916c3bb2902e6db
liember <liember@yandex.ru>
2020-02-22 02:53:40 +0700

    interpreter library

--- src/modules/interpreter/Decode.cpp
+++ src/modules/interpreter/Decode.cpp
@@ -1,36 +1,10 @@
-#include "commandHandler.hpp"
+#include "interpreter.hpp"
 #include <iostream>
 
-int commandHandler::Encode(int command, int operand, int *value)
-{
-    if (operand > 127)
-    {
-        std::cout << "[!] very long operand" << std::endl;
-        return 2;
-    }
-    bool command_is_defined = false;
-    for (int i = 0; i < 12; i++)
-    {
-        if (command == comands_list[i])
-        {
-            command_is_defined = true;
-        }
-    }
-    if (command_is_defined)
-    {
-        *value = 0;
-        *value = (command << 7) | operand;
-        return 0;
-    }
-    std::cout << "[!] undefined command" << std::endl;
-    return 1;
-}
-
-int commandHandler::Decode(int value, int *command, int *operand)
+int interpreter::Decode(int value, int *command, int *operand)
 {
     int attribute;
     int tmp_command, tmp_operand;
-
     attribute = (value >> 14) & 1;
     if (attribute == 0)
     {
