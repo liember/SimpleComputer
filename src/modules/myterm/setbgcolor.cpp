a847bb2e38d481377e8d8e09d4a390b87442fdd8
liember <liember@yandex.ru>
2020-03-24 19:15:15 +0700

    some update (remove switch)

--- src/modules/myterm/setbgcolor.cpp
+++ src/modules/myterm/setbgcolor.cpp
@@ -3,37 +3,6 @@
 
 int myTerm::SetBgColor(enum colors color)
 {
-	switch (color)
-	{
-	case black:
-		std::cout << "\E[40m";
-		break;
-	case red:
-		std::cout << "\E[41m";
-		break;
-	case green:
-		std::cout << "\E[42m";
-		break;
-	case brown:
-		std::cout << "\E[43m";
-		break;
-	case blue:
-		std::cout << "\E[44m";
-		break;
-	case magenta:
-		std::cout << "\E[45m";
-		break;
-	case cyan:
-		std::cout << "\E[46m";
-		break;
-	case light_blue:
-		std::cout << "\E[47m";
-		break;
-	case defaul:
-		std::cout << "\E[49m";
-		break;
-	default:
-		return -1;
-	}
+	std::cout << "\E[4" << color << "m";
 	return 0;
 }
--- src/modules/myterm/setfgcolor.cpp
+++ src/modules/myterm/setfgcolor.cpp
@@ -3,37 +3,6 @@
 
 int myTerm::SetFgColor(enum colors color)
 {
-	switch (color)
-	{
-	case black:
-		std::cout << "\E[30m";
-		break;
-	case red:
-		std::cout << "\E[31m";
-		break;
-	case green:
-		std::cout << "\E[32m";
-		break;
-	case brown:
-		std::cout << "\E[33m";
-		break;
-	case blue:
-		std::cout << "\E[34m";
-		break;
-	case magenta:
-		std::cout << "\E[35m";
-		break;
-	case cyan:
-		std::cout << "\E[36m";
-		break;
-	case light_blue:
-		std::cout << "\E[37m";
-		break;
-	case defaul:
-		std::cout << "\E[39m";
-		break;
-	default:
-		return -1;
-	}
+	std::cout << "\E[3" << color << "m";
 	return 0;
 }
