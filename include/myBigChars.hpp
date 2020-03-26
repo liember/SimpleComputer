8d244435a6bdaa532cc9eda5c267f2e5db523b37
liember <liember@yandex.ru>
2020-03-26 18:04:06 +0700

    logic fix

--- include/myBigChars.hpp
+++ include/myBigChars.hpp
@@ -16,21 +16,27 @@ private:
     myTerm *term;
 
 private:
+    // prints string using additional code table
     int PrintA(char *str);
-
-public:
-    myBigChars(myTerm *t);
-
-    int PrintBox(int x1, int y1, int x2, int y2);
-    int PrintBigChar(int *big, int x, int y, enum myTerm::colors fg, enum myTerm::colors bg);
 
     int SetBigCharPos(int *big, int x, int y, int value);
     int GetBigCharPos(int *big, int x, int y, int *value);
 
+public:
+    myBigChars(myTerm *t);
+
+    // prints box, just box ;)
+    int PrintBox(int x1, int y1, int x2, int y2);
+
+    // prints big char 8x8
+    int PrintBigChar(int *big, int x, int y, enum myTerm::colors fg, enum myTerm::colors bg);
+
+    //write 'count' bigchars 'big' into 'fd'
     int Write(int fd, int *big, int count);
+    // read bigchar from file
     int Read(int fd, int *big, int need_count, int *count);
 
     // specific func for transform array into big char
     // char bigchar_array[64] = {}
-    int *arr_to_big(char *arr);
+    int *ArrToBig(char *arr);
 };
