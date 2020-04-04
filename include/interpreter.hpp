bf2223ae672e5082b1c92512d6257c155bc29953
liember <liember@yandex.ru>
2020-04-04 16:54:45 +0700

    hex code of commands

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -1,17 +1,36 @@
 #pragma once
 
-#define READ 10
-#define WRITE 11
-#define LOAD 20
-#define STORE 21
-#define ADD 30
-#define SUB 31
-#define DIVIDE 32
-#define MUL 33
-#define JUMP 40
-#define JNEG 41
-#define JZ 42
-#define HALT 43
+#define READ 0x10
+#define WRITE 0x11
+
+#define LOAD 0x20
+#define STORE 0x21
+
+#define ADD 0x30
+#define SUB 0x31
+#define DIVIDE 0x32
+#define MUL 0x33
+
+#define JUMP 0x40
+#define JNEG 0x41
+#define JZ 0x42
+#define HALT 0x43
+
+enum comands
+{
+    Read = 0x10,
+    Write = 0x11,
+    load = 0x20,
+    store = 0x21,
+    add = 0x30,
+    sub = 0x31,
+    divide = 0x32,
+    mul = 0x33,
+    jump = 0x40,
+    jneg = 0x41,
+    jz = 0x42,
+    halt = 0x43
+};
 
 class interpreter
 {
