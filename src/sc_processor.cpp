16e05fdd93a11d18d454110405c0a1eb36cd2fee
liember <liember@yandex.ru>
2020-04-07 21:03:53 +0700

    Simple control unit for Simple Computer

--- src/sc_processor.cpp
+++ src/sc_processor.cpp
@@ -52,3 +52,77 @@ int SimpleComputer::ALU(int command, int operand)
     rg->Set(FLAG_ODD, acc->Get() & 1);
     return 0;
 }
+
+void SimpleComputer::CU()
+{
+    int command, operand;
+    int flag, read_suc;
+
+    int cmd_interpreted = interpreter.Decode(memory::Memory::mem[inst_counter], &command, &operand);
+
+    if (inst_counter >= memory::MEM_SIZE)
+    {
+        rg->Set(FLAG_OUTMEM, 1);
+        rg->Set(FLAG_INTERRUPT, 1);
+    }
+    if (cmd_interpreted)
+    {
+        rg->Set(FLAG_COMMAND, 1);
+        rg->Set(FLAG_INTERRUPT, 1);
+        return;
+    }
+    else if ((operand < 0) || (operand >= memory::MEM_SIZE))
+    {
+        rg->Set(FLAG_COMMAND, 1);
+        rg->Set(FLAG_INTERRUPT, 1);
+        return;
+    }
+    else if ((command >= 0x30) && (command <= 0x33))
+    {
+        if (ALU(command, operand) != 0)
+            rg->Set(FLAG_INTERRUPT, 1);
+    }
+    else
+    {
+        switch (command)
+        {
+        case comands::Read:
+            // do
+            // {
+            //     read_suc = read_mcell(operand);  sc_mem->
+            // } while (read_suc != 0);
+            break;
+
+        case comands::Write:
+            // write_used = 1;
+            // write_val = sc_memory[operand];
+            break;
+
+        case comands::load:
+            acc->Set(memory::Memory::mem[operand]);
+            break;
+
+        case comands::store:
+            memory::Memory::mem[operand] = acc->Get();
+            break;
+
+        case comands::jump:
+            inst_counter = operand - 1;
+            break;
+
+        case comands::jneg:
+            if (((acc->Get() >> 14) & 1) == 1)
+                inst_counter = operand - 1;
+            break;
+
+        case comands::jz:
+            if (acc->Get() == 0)
+                inst_counter = operand - 1;
+            break;
+
+        case comands::halt:
+            rg->Set(FLAG_INTERRUPT, 1);
+            break;
+        }
+    }
+}
