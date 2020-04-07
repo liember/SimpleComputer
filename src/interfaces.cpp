b934a10a4876eb6659fe529fd4e7e71bcef723c2
liember <liember@yandex.ru>
2020-04-07 21:09:13 +0700

    interfaces moved here

---
+++ src/interfaces.cpp
@@ -0,0 +1,28 @@
+#include "mem_extern.hpp"
+#include "mem_intern.hpp"
+#include "terminal.hpp"
+
+internal_memory::Interface::Interface()
+{
+    registers.Init();
+    accamulator.Set(0);
+    instruction_count.Set(0);
+}
+
+external_memory::Interface::Interface()
+{
+    ram.Init();
+}
+
+external_memory::Interface::Interface(int ram_size) : ram(ram_size)
+{
+    ram.Init();
+}
+
+// ############## terminal #####################
+
+void terminal::Interface::GoToLastRow()
+{
+    term.GotoXY(0, term.GetScreenSizeY() + 1);
+    std::cout << std::endl;
+}
