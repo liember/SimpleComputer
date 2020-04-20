1f399d6ef026eb9e4ac20553032253f86a1bd1df
liember <liember@yandex.ru>
2020-04-20 22:07:28 +0700

    use new namespaces for flags

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -3,12 +3,12 @@
 
 using namespace ALU;
 
-int Interpreter::Decode(const uint16_t value, uint8_t *command, uint8_t *operand)
+int Interpreter::Decode(const int16_t value, uint8_t *command, uint8_t *operand)
 {
     int attribute;
     int tmp_command, tmp_operand;
     attribute = (value >> 14) & 1;
-    if (attribute == 0)
+    if (attribute == 1)
     {
         tmp_command = (value >> 7) & 0x7F;
         tmp_operand = value & 0x7F;
@@ -28,20 +28,20 @@ int Interpreter::Decode(const uint16_t value, uint8_t *command, uint8_t *operand
         }
         else
         {
-            throw errors::undef_comand;
+            return errors::translation::undef_comand;
         }
     }
     else
     {
-        throw errors::non_comand_data;
+        return errors::translation::non_comand_data;
     }
     return 0;
 }
-int Interpreter::Encode(uint8_t command, uint8_t operand, uint16_t *value)
+int Interpreter::Encode(uint8_t command, uint8_t operand, int16_t *value)
 {
     if (operand > 127)
     {
-        throw errors::long_operand;
+        throw errors::translation::long_operand;
     }
 
     bool command_is_defined = true;
@@ -54,9 +54,10 @@ int Interpreter::Encode(uint8_t command, uint8_t operand, uint16_t *value)
     }
     if (!command_is_defined)
     {
-        throw errors::undef_comand;
+        throw errors::translation::undef_comand;
     }
     *value = 0;
     *value = (command << 7) | operand;
+    *value = *value | (uint16_t)0x4000;
     return 0;
 }
