1fa860b89149a4e939e91e80a6ab116c3b87c996
liember <liember@yandex.ru>
2020-04-20 22:08:10 +0700

    test init

--- src/SimpleComputer.cpp
+++ src/SimpleComputer.cpp
@@ -14,6 +14,16 @@ void SimpleComputer::Init()
     run_status = true;
     system_io.ClearScreen();
     system_io.DrawInterface();
+
+    external_mem.ram.memory[1] = (uint16_t)11;
+
+    external_mem.ram.memory[99] = (uint16_t)15;
+    external_mem.ram.memory[98] = (uint16_t)11234;
+
+    processor.translateor.Encode(ALU::comands::add, 1, &external_mem.ram.memory[0]);
+    internal_mem.registers.Set(internal_memory::flags::interrupt, true);
+
+    system_io.DrawInterface();
 }
 
 void SimpleComputer::End()
@@ -23,7 +33,19 @@ void SimpleComputer::End()
 
 void SimpleComputer::Process()
 {
-    system_io.ReadKey();
+    switch (system_io.ReadKey())
+    {
+    case user_interaction::states::run_until_end:
+        /* code */
+        break;
 
+    case user_interaction::states::run_until_next:
+        processor.Step();
+        break;
+
+    case user_interaction::states::exit:
+        run_status = false;
+        break;
+    }
     system_io.DrawInterface();
 }
