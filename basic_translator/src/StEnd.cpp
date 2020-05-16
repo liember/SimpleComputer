986bde4b63c88966b9dfe7d90e8e92706d54717c
liember <liember@yandex.ru>
2020-05-17 01:24:01 +0700

    add i_assembly code generation

--- basic_translator/src/StEnd.cpp
+++ basic_translator/src/StEnd.cpp
@@ -33,7 +33,20 @@ void EndStatement::SetAddr(int a)
     asm_address = a;
 }
 
-int EndStatement::GetAddr()
+int *EndStatement::GetAddr()
 {
-    return asm_address;
+    return &asm_address;
+}
+
+int EndStatement::GetId()
+{
+    return address;
+}
+
+std::vector<asmword *> *EndStatement::GenerateAsm(library::addressTable *variables, std::vector<parsing::AST::Statement *> *statements)
+{
+    std::vector<asmword *> *ret = new std::vector<asmword *>;
+    asmword *command = new asmword(&asm_address, "HALT", nullptr);
+    ret->push_back(command);
+    return ret;
 }
