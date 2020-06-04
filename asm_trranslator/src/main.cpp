8e9eafd4dee6563d238006d9d819d788f46b6e1f
liember <liember@yandex.ru>
2020-06-05 01:53:37 +0700

    uint to int

--- asm_trranslator/src/main.cpp
+++ asm_trranslator/src/main.cpp
@@ -1,229 +1,150 @@
-#include <iostream>
-#include <fstream>
 #include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include <vector>
+const int16_t standart_line_size = 100;
+const int16_t max_mem = 100;
+const int16_t read = 0x10, write = 0x11,
+              load = 0x20,
+              store = 0x21,
+              add = 0x30,
+              sub = 0x31,
+              divide = 0x32,
+              mul = 0x33,
+              jump = 0x40,
+              jneg = 0x41,
+              jz = 0x42,
+              halt = 0x43,
+              AND = 0x52,
+              Hal = 0x60,
+              SET_VARIABLE = 0xfff;
 
-#include "StringSwitch.hpp"
-
-using namespace std;
-
-constexpr int max_mem = 100;
-
-enum comands_code
+typedef struct asm_cmd
 {
-    read = 0x10,
-    write = 0x11,
-    load = 0x20,
-    store = 0x21,
-    add = 0x30,
-    sub = 0x31,
-    divide = 0x32,
-    mul = 0x33,
-    jump = 0x40,
-    jneg = 0x41,
-    jz = 0x42,
-    halt = 0x43,
-    AND = 0x52,
-    SET_VARIABLE
-};
+    int16_t addres;
+    int16_t comand;
+    int16_t operand;
+} asm_cmd;
 
-int check_list_size = 13;
-static constexpr uint8_t check_list[] = {comands_code::read,
-                                         comands_code::write,
-                                         comands_code::load,
-                                         comands_code::store,
-                                         comands_code::add,
-                                         comands_code::sub,
-                                         comands_code::divide,
-                                         comands_code::mul,
-                                         comands_code::jump,
-                                         comands_code::jneg,
-                                         comands_code::jz,
-                                         comands_code::halt,
-                                         comands_code::AND};
-
-int Encode(uint8_t command, uint8_t operand, uint16_t *value)
+int Encode(int16_t prevaddr, asm_cmd *asm_cmd, int16_t *value)
 {
-    if (operand > max_mem)
-    {
-        cout << "[!] Operand address exceeds memory size" << endl;
-        return 1;
-    }
-
-    bool command_is_defined = true;
-    for (int i = 0; i < check_list_size; i++)
-    {
-        if (command == check_list[i])
-        {
-            command_is_defined = true;
-        }
-    }
-    if (!command_is_defined)
-    {
-        cout << "[!] Unsupported command" << endl;
-        return 1;
-    }
-    *value = 0;
-    *value = (command << 7) | operand;
-    *value = *value | (uint16_t)0x4000;
+    if (prevaddr >= asm_cmd->addres)
+        perror("АДРЕСАЦИЯ ХУЕВАЯ ЕБЛАН"), exit(1);
+    if (asm_cmd->comand == SET_VARIABLE)
+        if (asm_cmd->operand > 0x3fff || asm_cmd->operand < -0x3fff)
+            perror("ЧИСЛО ОГРОМНОЕ"), exit(1);
+        else
+            *value = asm_cmd->operand;
+    else if (asm_cmd->operand < max_mem)
+        *value = ((asm_cmd->comand << 7) | asm_cmd->operand) | 0x4000;
+    else
+        perror("АДДРЕС В ОПЕРАНДЕ БОЛЬШЕ ПАМЯТИ УСТРОЙСТВА"), exit(1);
     return 0;
 }
 
-struct order_t
+asm_cmd *AddCmdCode(asm_cmd *asm_cmd, const char *cmd)
 {
-    uint16_t addres;
-    string comand;
-    uint8_t operand;
-    order_t(uint16_t a, string b, uint8_t c) : addres(a), comand(b), operand(c) {}
-};
+    if (!strcmp(cmd, "READ"))
+        asm_cmd->comand = read;
+    else if (!strcmp(cmd, "HALT"))
+        asm_cmd->comand = halt;
+    else if (!strcmp(cmd, "AND"))
+        asm_cmd->comand = AND;
+    else if (!strcmp(cmd, "HALT"))
+        asm_cmd->comand = Hal;
+    else if (!strcmp(cmd, "WRITE"))
+        asm_cmd->comand = write;
+    else if (!strcmp(cmd, "LOAD"))
+        asm_cmd->comand = load;
+    else if (!strcmp(cmd, "STORE"))
+        asm_cmd->comand = store;
+    else if (!strcmp(cmd, "ADD"))
+        asm_cmd->comand = add;
+    else if (!strcmp(cmd, "SUB"))
+        asm_cmd->comand = sub;
+    else if (!strcmp(cmd, "DIVIDE"))
+        asm_cmd->comand = divide;
+    else if (!strcmp(cmd, "MUL"))
+        asm_cmd->comand = mul;
+    else if (!strcmp(cmd, "JUMP"))
+        asm_cmd->comand = jump;
+    else if (!strcmp(cmd, "JNEG"))
+        asm_cmd->comand = jneg;
+    else if (!strcmp(cmd, "JZ"))
+        asm_cmd->comand = jz;
+    else if (!strcmp(cmd, "="))
+        asm_cmd->comand = SET_VARIABLE;
+    else
+        printf("%s НЕТ ТАКОЙ КОМАНДЫ", cmd), exit(1);
+    return asm_cmd;
+}
 
-struct asm_cmd
+asm_cmd ToAsmCmdStructure(char *buf)
 {
-    uint16_t addres;
-    uint8_t comand;
-    uint8_t operand;
-    asm_cmd(uint16_t a, uint8_t b, uint8_t c) : addres(a), comand(b), operand(c) {}
-};
-
-struct asm_var
-{
-    uint16_t value;
-    uint16_t addres;
-};
-
-asm_cmd Translate(order_t *ord)
-{
-    asm_cmd cmd(0, 0, 0);
-    ss_switch(ord->comand)
+    asm_cmd cmd;
+    char *pch = strtok(buf, " ");
+    int word_counter = 0;
+    while (pch != NULL)
     {
-        ss_case("READ") : cmd.comand = comands_code::read;
-        break;
-        ss_case("WRITE") : cmd.comand = comands_code::write;
-        break;
-        ss_case("LOAD") : cmd.comand = comands_code::load;
-        break;
-        ss_case("STORE") : cmd.comand = comands_code::store;
-        break;
-        ss_case("ADD") : cmd.comand = comands_code::add;
-        break;
-        ss_case("SUB") : cmd.comand = comands_code::sub;
-        break;
-        ss_case("DIVIDE") : cmd.comand = comands_code::divide;
-        break;
-        ss_case("MUL") : cmd.comand = comands_code::mul;
-        break;
-        ss_case("JUMP") : cmd.comand = comands_code::jump;
-        break;
-        ss_case("JNEG") : cmd.comand = comands_code::jneg;
-        break;
-        ss_case("JZ") : cmd.comand = comands_code::jz;
-        break;
-        ss_case("HALT") : cmd.comand = comands_code::halt;
-        break;
-        ss_case("AND") : cmd.comand = comands_code::AND;
-        break;
-        ss_case("=") : cmd.comand = comands_code::SET_VARIABLE;
-        break;
+        ++word_counter;
+        if (pch[0] == ';' && word_counter < 4)
+            perror("КОМЕНТАРИЙ НЕ К МЕСТУ"), exit(1);
+        switch (word_counter)
+        {
+        case 1:
+            cmd.addres = atoi(pch);
+            if (cmd.addres > max_mem)
+                perror("ADDRES MORE MEMORY"), exit(1);
+            break;
+        case 2:
+            cmd = *AddCmdCode(&cmd, pch);
+            break;
+        case 3:
+            cmd.operand = atoi(pch);
+            break;
+        case 4:
+            if (pch[0] != ';')
+                perror(" ЛИШНИЕ БУКВЫ УБЕРИ БЛЯТЬ"), exit(1);
+            break;
+        }
+        pch = strtok(NULL, " ");
     }
-    cmd.operand = ord->operand;
-    cmd.addres = ord->addres;
     return cmd;
 }
 
 int main(int argc, char **argv)
 {
+    unsigned int comands_counter = 0;
+    int prev_addr = -1;
+    char buf[standart_line_size];
+    int16_t program[max_mem];
 
-    vector<order_t> program;
-    uint16_t max_addr = -1;
+    for (size_t i = 0; i < max_mem; i++)
+        program[i] = 0;
 
-    std::ifstream in("testcode.asm");
-    if (in.is_open())
+    FILE *fp = fopen("test", "r");
+    if (fp != NULL)
     {
-
-        int addres;
-        string comand;
-        int operand;
-
-        cout << "[?] Start translation:" << endl
-             << "#####################" << endl;
-
-        while (in >> addres >> comand >> std::hex >> operand)
-        {
-            if (addres < max_mem)
+        while (!feof(fp))
+            if (fgets(buf, standart_line_size, fp) != NULL)
             {
-                if (addres > max_addr)
-                {
-                    max_addr = addres;
-                }
-
-                cout << "Order with:" << endl
-                     << "addres : " << addres << endl
-                     << "comand: " << comand << endl
-                     << "operand: " << operand << endl;
-                cout << " ------------------------ " << endl;
-                program.push_back(order_t(addres, comand, operand));
+                int16_t temp_cmd;
+                asm_cmd lol = ToAsmCmdStructure(buf);
+                if (!Encode(prev_addr, &lol, &temp_cmd))
+                    program[lol.addres] = temp_cmd, ++comands_counter;
+                else
+                    perror("ПРОБЛЕМНЫЙ КОД У ТЕБЯ СУКА"), exit(1);
+                prev_addr = lol.addres;
             }
-            else
-            {
-                cout << "[!] Addres [" << addres << "] more than device memory" << endl;
-                cout << "[ Info ]" << endl
-                     << " ADDRES: " << addres
-                     << endl
-                     << " COMAND: " << comand
-                     << endl
-                     << " OPERAND: " << operand
-                     << endl;
-            }
-        }
+        FILE *out = fopen("test.bin", "wb");
+        if (out != NULL)
+            fwrite(program, sizeof(int16_t), max_mem, out);
+        else
+            perror("НЕ МОГУ ФАЙЛ СОЗДАТЬ");
+        fclose(out);
     }
     else
-    {
-        cout << "[!] File not found" << endl;
-    }
-
-    in.close();
-
-    if (program.size() > max_mem)
-    {
-        cout << "[!] Program more than device memory" << endl;
-        return 1;
-    }
-
-    uint16_t asmcode[max_addr];
-    for (size_t i = 0; i < program.size(); i++)
-    {
-        uint16_t code;
-
-        asm_cmd cmd = Translate(&program[i]);
-
-        if (cmd.comand == comands_code::SET_VARIABLE)
-        {
-            asmcode[cmd.addres] = cmd.operand;
-        }
-        else if (Encode(cmd.comand, cmd.operand, &code) == 0)
-        {
-            asmcode[cmd.addres] = code;
-        }
-        else
-        {
-            cout << "[ Info ]" << endl
-                 << " ADDRES: " << program[i].addres
-                 << endl
-                 << " COMAND: " << program[i].comand
-                 << endl
-                 << " OPERAND: " << program[i].operand
-                 << endl;
-        }
-    }
-
-    std::ofstream out("testcode.bin", ios::binary | ios::out);
-    if (out.is_open())
-    {
-        for (int i = 0; i < program.size(); i++)
-        {
-            out.write((char *)&asmcode[i], sizeof(uint16_t));
-        }
-    }
-    out.close();
+        perror("NET TAKOGA FAILA");
+    fclose(fp);
 }
